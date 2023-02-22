5ba0af501e6d9051f380d4e45f460c8262f7b97c
Seronia <51925751+2eronia@users.noreply.github.com>
2023-02-22 19:06:07 +0800

    Update DNS_test_whileLoop.sh

--- DNS_test_whileLoop.sh
+++ DNS_test_whileLoop.sh
@@ -3,6 +3,6 @@ while read -r line; do
     echo
     line="$(echo "$line" | tr -d '\r\n')"
     echo "@$line"
-    doggo baidu.com --time "@$line"
+    doggo baidu.com --time --timeout 1 "@$line"
     echo
 done < DNS.txt
