96bce42fe237f45276b365e61f31e872a7594bb3
Seronia <51925751+2eronia@users.noreply.github.com>
2023-02-22 19:05:48 +0800

    Update DNS_test_catLoop.sh

--- DNS_test_catLoop.sh
+++ DNS_test_catLoop.sh
@@ -3,6 +3,6 @@ cat file.txt | while read line; do
     echo
     line="$(echo "$line" | tr -d '\r\n')"
     echo "@$line"
-    doggo baidu.com --time "@$line"
+    doggo baidu.com --time --timeout 1 "@$line"
     echo
 done < DNS.txt
